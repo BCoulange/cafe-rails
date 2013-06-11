520c6dad9686251c7ddc98a3cc7ad5d1867ae61d
Baptiste Coulange <bc@cornis.fr>
2013-06-11 18:23:43 +0200

    scheduler task

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,5 +1,46 @@
 class Feed < ActiveRecord::Base
-  attr_accessible :name, :url
+	require 'feedzirra'
 
-  has_many :articles
+  	attr_accessible :name, :url
+
+  	has_many :articles
+
+
+  	def self.update
+  		Feed.all.each do |feed|
+          # get feed
+          puts "[Feed.update] updating #{feed.name}..."
+        	fzir = Feedzirra::Feed.fetch_and_parse(feed.url)
+        	
+          # get new articles
+          @this_feed_articles = []
+      		unless fzir.is_a?(Fixnum) || fzir.nil?
+        		fzir.entries.each do |e|
+        			@article = Article.new
+        			@article.author = e.author       			
+        			@article.categories = e.categories
+        			@article.content = e.content
+        			@article.published = e.published
+        			@article.summary = e.summary
+        			@article.title = e.title
+        			@article.url = e.url
+              @this_feed_articles << @article
+              @article.save
+        		end
+            # update feed
+            @old_articles = feed.articles
+            feed.articles = @this_feed_articles
+            feed.save
+
+            # destroy old articles
+            @old_articles.each do |article|
+              article.destroy
+            end 
+
+      		end			
+
+  		end	
+
+  	end
+
 end

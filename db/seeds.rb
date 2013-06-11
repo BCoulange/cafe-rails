7923ff672caa2b7b619085d32fb4a128366feddc
Baptiste Coulange <bc@cornis.fr>
2013-06-11 17:24:24 +0200

    creating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Feed.create(name: "Recherche en cours", url: "http://www.rechercheencours.fr/REC/Blogcast/rss.xml")
+Feed.create(name: "A la main", url: "http://www.alamain.info/?feed=rss2")
+Feed.create(name: "A la source" , url: "http://alasource.blog.lemonde.fr/feed/")
+Feed.create(name: "Anthropoblog" , url: "http://www.anthropoblog.fr/?feed=rss2")
+Feed.create(name: "Association Café des Sciences" , url: "http://association.cafe-sciences.org/feed/rss2")
+Feed.create(name: "Biopsci" , url: "http://feeds2.feedburner.com/Biopsci")
+Feed.create(name: "Xenius" , url: "http://xenius.arte.tv/blog/category/actualites/feed")	
+Feed.create(name: "drericsimon" , url: "http://drericsimon.blogspot.com/feeds/posts/default?alt=rss")
+Feed.create(name: "Choux Romanesco, vache qui rit et intégrales curvilignes" , url: "http://eljjdx.canalblog.com/rss.xml")
+Feed.create(name: "Du coté de chez elysa chlorotica" , url: "http://du-cote-de-chez-elysia-chlorotica.blogspot.com/feeds/posts/default?alt=rss")
+Feed.create(name: "Sciences le monde" , url: "http://sciences.blog.lemonde.fr/feed/")
+Feed.create(name: "Homo Fabulus" , url: "http://feeds.feedburner.com/HomoFabulus")
+Feed.create(name: "IdPhys" , url: "http://blog.idphys.fr/category/blog-2/feed/")
+Feed.create(name: "Strip Science" , url: "http://stripscience.cafe-sciences.org/articles/category/inedit-strip-science/feed/")
+Feed.create(name: "KidiScience" , url: "http://kidiscience.cafe-sciences.org/feed/")
+Feed.create(name: "Le blogue de Valérie Borde" , url: "http://www.lactualite.com/opinions/le-blogue-de-valerie-borde/feed/")
+Feed.create(name: "Dvawn" , url: "http://dvanw.blogspot.com/feeds/posts/default/-/sciences")

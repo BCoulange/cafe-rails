cca0716ed5a63646c78a8a1ddc9cdd2aab24b468
Baptiste Coulange <bc@cornis.fr>
2013-06-11 17:05:37 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CafeRails::Application.config.secret_token = 'f46c00c8aacb7f972469465bc7598150a7da16929e89ddf877a3b0d3081165a48b36409eba6325ef56f066667e27f0c347d923187d0323179aec3bb8611e7162'

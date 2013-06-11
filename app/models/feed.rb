233a2c4f44812e4576ae602ebeb6ca75f1da1fb7
Baptiste Coulange <bc@cornis.fr>
2013-06-11 22:27:11 +0200

    test sur heroku

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -39,14 +39,14 @@ class Feed < ActiveRecord::Base
               @article.feed_id = feed.id
               @this_feed_articles << @article
 
-              puts @article.author.size            
-              puts @article.categories.size
-              puts @article.content.size
-              puts @article.published.size
-              puts @article.summary.size
-              puts @article.title.size
-              puts @article.url.size
-              puts @article.feed_id.size
+              puts "@article.author.size #{@article.author.size}" unless @article.author.size.nil?           
+              puts "@article.categories.size #{@article.categories.size}" unless @article.categories.size.nil?
+              puts "@article.content.size #{@article.content.size}" unless @article.content.size.nil?
+              puts "@article.published.size #{@article.published.size}" unless @article.published.size.nil?
+              puts "@article.summary.size #{@article.summary.size}" unless @article.summary.size.nil?
+              puts "@article.title.size #{@article.title.size}" unless @article.title.size.nil?
+              puts "@article.url.size #{@article.url.size}" unless @article.url.size.nil?
+              puts "@article.feed_id.size #{@article.feed_id.size}" unless @article.feed_id.size.nil?
 
               @article.save
 
